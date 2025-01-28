c6feb6d51707bc50de374c4e8116dee2d51ecc24
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2025-01-27 21:11:05 -0300

    feat: create ProductRepository and UserRepository

---
+++ src/main/java/com/market/market/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.market.market.repositories;
+
+import com.market.market.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
