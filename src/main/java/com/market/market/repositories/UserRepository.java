4fefeb098c9ec4c02a7fe4e94e9acaa7452485e0
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2025-01-30 22:27:17 -0300

    feat: create findByUsername, findByCPF, and findByEmail methods

--- src/main/java/com/market/market/repositories/UserRepository.java
+++ src/main/java/com/market/market/repositories/UserRepository.java
@@ -4,6 +4,11 @@ import com.market.market.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByCPF(String CPF);
+    Optional<User> findByEmail(String email);
 }
