e1da0c08b62be8ccec7f802ca790e51fbd4c5857
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2025-01-30 22:30:51 -0300

    feat: create findByEmail method

--- src/main/java/com/market/market/controllers/UserController.java
+++ src/main/java/com/market/market/controllers/UserController.java
@@ -1,16 +1,18 @@
 package com.market.market.controllers;
 
+import com.market.market.dtos.UserDTO;
 import com.market.market.models.User;
 import com.market.market.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
     @Autowired
     UserService userService;
@@ -22,6 +24,12 @@ public class UserController {
         return ResponseEntity.ok().body(obj);
     }
 
+    @GetMapping("/username/{username}")
+    public ResponseEntity<User> findByUsername(@PathVariable String username) throws Exception {
+        User obj = this.userService.findUserByUsername(username);
+        return ResponseEntity.ok().body(obj);
+    }
+
     @GetMapping("/CPF/{CPF}")
     public ResponseEntity<User> findByCPF(@PathVariable String CPF) throws Exception {
         User obj = this.userService.findUserByCPF(CPF);
@@ -29,8 +37,16 @@ public class UserController {
     }
 
     @GetMapping("/username/{username}")
-    public ResponseEntity<User> findByUsername(@PathVariable String username) throws Exception {
-        User obj = this.userService.findUserByUsername(username);
+    public ResponseEntity<User> findByEmail(@PathVariable String email) throws Exception {
+        User obj = this.userService.findUserByEmail(email);
         return ResponseEntity.ok().body(obj);
+    }
+
+    // POST MAPPINGS
+    @Validated
+    @PostMapping
+    public ResponseEntity<User> createUser(@Valid @RequestBody UserDTO userDTO) throws Exception{
+        User user = this.userService.createUser(userDTO);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/market/market/services/UserService.java
+++ src/main/java/com/market/market/services/UserService.java
@@ -1,10 +1,12 @@
 package com.market.market.services;
 
+import com.market.market.dtos.UserDTO;
 import com.market.market.exceptions.UserNotFoundException;
 import com.market.market.models.User;
 import com.market.market.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -15,13 +17,26 @@ public class UserService {
         return this.userRepository.findById(id).orElseThrow(UserNotFoundException::new);
     }
 
-    public User findUserByCPF(String CPF) throws Exception {
-        return this.userRepository.findByCPF(CPF).orElseThrow(UserNotFoundException::new);
-    }
-
     public User findUserByUsername(String username) throws Exception {
         return this.userRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
     }
 
+    public User findUserByCPF(String CPF) throws Exception {
+        return this.userRepository.findByCPF(CPF).orElseThrow(UserNotFoundException::new);
+    }
 
+    public User findUserByEmail(String email) throws Exception {
+        return this.userRepository.findByEmail(email).orElseThrow(UserNotFoundException::new);
+    }
+
+    public void saveUser(User user) {
+        this.userRepository.save(user);
+    }
+
+    @Transactional
+    public User createUser(UserDTO userData) throws Exception{
+        User user = new User(userData);
+        saveUser(user);
+        return user;
+    }
 }
