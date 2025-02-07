ec8038c953cfea2e62cf11ddc1dcfc1daa903cb4
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2025-02-07 03:09:29 -0300

    feat: create quantity attribute for UpdateProductDTO

--- src/main/java/com/market/market/dtos/UpdateProductDTO.java
+++ src/main/java/com/market/market/dtos/UpdateProductDTO.java
@@ -2,9 +2,11 @@ package com.market.market.dtos;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
 import java.math.BigDecimal;
 
 public record UpdateProductDTO(
-        @NotNull(message = "{price.NotNull}") @Positive(message = "{price.Positive}") BigDecimal price) {
+        @NotNull(message = "{price.NotNull}") @Positive(message = "{price.Positive}") BigDecimal price,
+        @NotNull(message = "{quantity.NotNull}") @PositiveOrZero(message = "{quantity.PositiveOrZero}") Integer quantity) {
 }
