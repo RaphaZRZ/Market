3bdbb9ee360336de8c2c0ce27d4652407a96852c
RaphaZ <115039334+RaphaZRZ@users.noreply.github.com>
2025-02-11 16:22:23 -0300

    feat: create MethodArgumentNotValidException and ConstraintViolationException

--- src/main/java/com/market/market/configs/ControllerExceptionHandler.java
+++ src/main/java/com/market/market/configs/ControllerExceptionHandler.java
@@ -2,7 +2,9 @@ package com.market.market.configs;
 
 import com.market.market.dtos.ExceptionDTO;
 import com.market.market.exceptions.UserNotFoundException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,6 +14,26 @@ public class ControllerExceptionHandler {
     public ResponseEntity<ExceptionDTO> handleUserNotFoundException(UserNotFoundException exception) {
         ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), exception.getStatusCode());
         return ResponseEntity.status(exception.getStatusCode()).body(exceptionDTO);
+    }
+
+    // Validate BEAN annotations during save or update of data in the database
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ExceptionDTO> handleValidationException(ConstraintViolationException exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Validation error: " + exception.getMessage(), 400);
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    // Validate BEAN annotations when an object is invalid as an argument of an endpoint
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionDTO> handleBeanValidationException(MethodArgumentNotValidException exception) {
+        StringBuilder errorMessage = new StringBuilder("Validation errors: ");
+        exception.getBindingResult().getAllErrors().forEach(error -> {
+            String message = error.getDefaultMessage();
+            errorMessage.append(String.format("%s; ", message));
+        });
+
+        ExceptionDTO exceptionDTO = new ExceptionDTO(errorMessage.toString(), 400);
+        return ResponseEntity.badRequest().body(exceptionDTO);
     }
 
     // Generals exceptions
